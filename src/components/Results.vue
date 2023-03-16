92e4802afda2de21d105d729239de2a5a59dd4da
unknown <huzitaiwo@gmail.com>
2023-03-16 21:34:38 +0100

    completed Results component

---
+++ src/components/Results.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="reaction-time">
+    <p>Reaction time: {{ score }}ms</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Results',
+  props: ['score']
+}
+</script>
+
+<style>
+
+</style>
